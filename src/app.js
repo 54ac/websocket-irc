fe49d78090a20a36f8e047b1f52a50aa66629e3e
rowrawer <rowrawer@users.noreply.github.com>
2019-02-16 03:13:39 +0100

    1

---
+++ src/app.js
@@ -0,0 +1,340 @@
+const http = require("http");
+const ws = require("ws");
+const express = require("express");
+const logger = require("morgan");
+const MongoClient = require("mongodb").MongoClient;
+//const assert = require("assert");
+const bcrypt = require("bcrypt");
+
+const app = express();
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname));
+app.set("view options", { layout: false });
+
+const port = process.env.PORT || "80";
+app.set("port", port);
+
+const server = http.createServer(app);
+const wss = new ws.Server({ server });
+
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
+
+app.get("/", (req, res, next) => {
+	res.render("index");
+});
+
+const mongoURL = "mongodb://localhost:27017/chat";
+var dbo;
+
+MongoClient.connect(
+	mongoURL,
+	{ useNewUrlParser: true },
+	function(err, db) {
+		if (err) throw err;
+		dbo = db.db("chat");
+		dbo.createCollection("messages", function(err, res) {
+			if (err) throw err;
+		});
+		dbo.createCollection("lifeforms", function(err, res) {
+			if (err) throw err;
+		});
+		console.log("mongo working");
+	}
+);
+
+const defaultChannel = "general";
+
+var channels = { [defaultChannel]: [] }; //list of people in a given channel
+var loggedIn = []; //list of people online
+
+wss.on("connection", ws => {
+	ws.on("message", message => {
+		let splitMessage = message.split(" ", 2);
+
+		function sendLog(channel) {
+			dbo
+				.collection("messages")
+				.find({ channel: channel }, { _id: 0 })
+				.each((err, msg) => {
+					if (err) throw err;
+					if (msg)
+						ws.send(`[${msg.timeStamp}] ${msg.userName}: ${msg.message}`);
+				});
+		}
+
+		function postLogIn(channel) {
+			ws.channel = channel;
+			if (channels[channel].includes(ws.userName.toLowerCase())) {
+				//no duplicate logins
+				ws.userName = "";
+				ws.channel = "";
+				ws.passwd = "";
+				ws.send("nah");
+			} else {
+				if (!channels[ws.channel]) channels[ws.channel] = [];
+				loggedIn.push(ws.userName.toLowerCase());
+				channels[ws.channel].push(ws.userName);
+				ws.send("channel: " + ws.channel);
+				wss.clients.forEach(client => {
+					if (client !== ws && client.channel === ws.channel) {
+						console.log("sent list request");
+						client.send("list: " + channels[ws.channel].join(" "));
+					}
+				});
+				console.log(ws.userName + " logged in");
+			}
+		}
+		//login segment
+		if (splitMessage[0] === "/name") {
+			//username handling
+			if (loggedIn.includes(ws.userName) && ws.channel) {
+				// if this is a name change request then do that
+				let oldName = ws.userName.toLowerCase();
+				ws.userName = splitMessage[1].substr(23);
+				dbo
+					.collection("lifeforms")
+					.findOne(
+						{ userName: ws.userName.toLowerCase() },
+						{ _id: 0 },
+						(err, result) => {
+							if (err) throw err;
+							if (!result) {
+								// if there is no other existing account by that name
+								//update the arrays as well
+								dbo
+									.collection("lifeforms")
+									.updateOne(
+										{ userName: oldName },
+										{ $set: { userName: ws.userName.toLowerCase() } },
+										(err, res) => {
+											if (err) throw err;
+											channels[ws.channel].splice(
+												channels[ws.channel].indexOf(oldName),
+												1
+											);
+											channels[ws.channel].push(ws.userName.toLowerCase());
+											loggedIn.splice(loggedIn.indexOf(oldName), 1);
+											loggedIn.push(ws.userName.toLowerCase());
+											ws.send("name changed to " + ws.userName);
+											console.log(
+												oldName + " changed their name to " + ws.userName
+											);
+										}
+									);
+							} else ws.send("same name");
+						}
+					);
+			} else if (!splitMessage[1].match(/[^\S]+/)) {
+				ws.userName = splitMessage[1];
+			}
+		} else if (ws.userName && splitMessage[0] === "/passwd") {
+			//password handling
+			ws.passwd = splitMessage[1];
+			dbo
+				.collection("lifeforms")
+				.findOne(
+					{ userName: ws.userName.toLowerCase() },
+					{ _id: 0 },
+					(err, result) => {
+						if (err) throw err;
+						if (result) {
+							//if it's an existing user
+							if (
+								loggedIn.includes(result.userName.toLowerCase() && ws.channel)
+							) {
+								//if the person's logged in then change their password when they send the command
+								bcrypt.hash(ws.passwd, 8, (err, res) => {
+									if (err) throw err;
+									if (res) {
+										dbo
+											.collection("lifeforms")
+											.updateOne(
+												{ userName: result.userName },
+												{ $set: { passwd: res } },
+												(err, res) => {
+													if (err) throw err;
+													console.log(
+														result.userName + " changed their password"
+													);
+													ws.send("password changed");
+												}
+											);
+									}
+								});
+							} else {
+								bcrypt.compare(ws.passwd, result.passwd, (err, res) => {
+									if (err) throw err;
+									if (res) {
+										//avoid duplicate logins
+										postLogIn(result.defaultChannel);
+									} else {
+										ws.userName = "";
+										ws.channel = "";
+										ws.passwd = "";
+										ws.send("nah");
+									}
+								});
+							}
+						} else {
+							//if it's a new user then create account
+							bcrypt.hash(ws.passwd, 8, (err, result) => {
+								if (err) throw err;
+								let messageObj = {
+									userName: ws.userName.toLowerCase(),
+									passwd: result,
+									defaultChannel: defaultChannel
+								};
+								dbo
+									.collection("lifeforms")
+									.insertOne(messageObj, function(err, res) {
+										if (err) throw err;
+										console.log("added " + messageObj.userName);
+									});
+								postLogIn(messageObj.defaultChannel);
+							});
+						}
+					}
+				);
+		} else if (
+			loggedIn.includes(ws.userName.toLowerCase()) &&
+			(splitMessage[0] === "/join" || splitMessage[0] === "/j")
+		) {
+			if (splitMessage[1].toLowerCase() !== ws.channel) {
+				//only do things if it's a different channel
+				channels[ws.channel].splice(
+					channels[ws.channel].indexOf(ws.userName.toLowerCase()),
+					1
+				);
+				ws.channel = splitMessage[1].toLowerCase();
+				console.log(ws.userName + " changed channel to #" + ws.channel);
+				ws.send("channel: " + ws.channel);
+				if (!channels[ws.channel]) channels[ws.channel] = [];
+				channels[ws.channel].push(ws.userName.toLowerCase());
+				wss.clients.forEach(client => {
+					if (client !== ws && client.channel === ws.channel) {
+						console.log("sent list request");
+						client.send("list: " + channels[ws.channel].join(" "));
+					}
+				});
+			} else {
+				ws.send("same channel");
+			}
+		} else if (
+			loggedIn.includes(ws.userName.toLowerCase()) &&
+			message === "logs???"
+		) {
+			sendLog(ws.channel);
+		} else if (
+			loggedIn.includes(ws.userName.toLowerCase()) &&
+			message === "/default"
+		) {
+			dbo
+				.collection("lifeforms")
+				.updateOne(
+					{ userName: ws.userName.toLowerCase() },
+					{ $set: { defaultChannel: ws.channel } },
+					(err, res) => {
+						if (err) throw err;
+						console.log(
+							ws.userName + " changed their default channel to #" + ws.channel
+						);
+						ws.send("default channel set");
+					}
+				);
+		} else if (
+			loggedIn.includes(ws.userName.toLowerCase()) &&
+			message === "/list"
+		) {
+			ws.send("list: " + channels[ws.channel].join(" "));
+		} else if (
+			loggedIn.includes(ws.userName.toLowerCase()) &&
+			splitMessage[0] === "/w"
+		) {
+			ws.timeStamp = new Date();
+			wss.clients.forEach(client => {
+				if (client.userName === splitMessage[1]) {
+					client.send(
+						`[${ws.timeStamp.getTime()}] [w] ${ws.userName}: ${message.substr(
+							splitMessage[0].length + splitMessage[1].length + 2
+						)}`
+					);
+					ws.send(
+						`[${ws.timeStamp.getTime()}] [w] ${ws.userName}: ${message.substr(
+							splitMessage[0].length + splitMessage[1].length + 2
+						)}`
+					);
+				}
+			});
+		} else if (
+			loggedIn.includes(ws.userName.toLowerCase()) &&
+			splitMessage[0] === "/msg"
+		) {
+			//message handling
+			ws.timeStamp = new Date();
+			let messageObj = {
+				timeStamp: ws.timeStamp.getTime(),
+				channel: ws.channel,
+				userName: ws.userName,
+				message: message.substr(splitMessage[0].length + 1)
+			};
+			dbo.collection("messages").insertOne(messageObj, function(err, res) {
+				if (err) throw err;
+				console.log(
+					`#${messageObj.channel} ${
+						messageObj.userName
+					}: ${messageObj.message.substr(0, 10)} received and inserted into db`
+				);
+			});
+			wss.clients.forEach(client => {
+				if (client !== ws && client.channel === ws.channel) {
+					client.send(
+						`[${ws.timeStamp.getTime()}] ${ws.userName}: ${message.substr(
+							splitMessage[0].length + 1
+						)}`
+					);
+				}
+			});
+		}
+	});
+	ws.on("close", function close() {
+		if (ws.userName) {
+			if (ws.channel)
+				channels[ws.channel].splice(
+					channels[ws.channel].indexOf(ws.userName.toLowerCase()),
+					1
+				);
+			loggedIn.splice(loggedIn.indexOf(ws.userName), 1);
+			console.log(ws.userName + " disconnected");
+		}
+	});
+});
+
+function onError(error) {
+	if (error.syscall !== "listen") {
+		console.error(error);
+	}
+
+	const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+	// handle specific listen errors with friendly messages
+	switch (error.code) {
+		case "EACCES":
+			console.error(bind + " requires elevated privileges");
+			process.exit(1);
+			break;
+		case "EADDRINUSE":
+			console.error(bind + " is already in use");
+			process.exit(1);
+			break;
+		default:
+			console.error(error);
+	}
+}
+
+function onListening() {
+	console.log(`http server up on ${server.address().port}`);
+}
---
+++ src/main.js
@@ -0,0 +1,231 @@
+"use strict";
+
+const webSocket = window.WebSocket || window.MozWebSocket;
+const chat = new Terminal();
+const userlist = new Terminal();
+var username;
+var currentChannel;
+var chatSocket = new webSocket(`ws://${location.host}`);
+
+chat.setBackgroundColor("white");
+chat.setTextColor("black");
+chat.setWidth("100%");
+chat.setHeight("100vh");
+
+userlist.setBackgroundColor("white");
+userlist.setTextColor("black");
+userlist.setWidth("100%");
+userlist.setHeight("100vh");
+
+function getTimeStamp() {
+	let timeStamp = new Date();
+	timeStamp = `${("0" + timeStamp.getHours()).substr(-2)}:${(
+		"0" + timeStamp.getMinutes()
+	).substr(-2)}`;
+	return timeStamp;
+}
+
+function login(no) {
+	clearInterval();
+	userlist.clear();
+	if (no) chat.print("// no spaces allowed!!!");
+	chat.input("name???", output => {
+		username = output.substr(0, 23);
+		if (username.match(/[^\S]+/)) {
+			chat.clear();
+			login(true);
+		} else {
+			chatSocket.send("/name " + username);
+			chat.print("---");
+			chat.password("password???", output => {
+				chatSocket.onmessage = event => {
+					if (event.data.startsWith("channel: ")) {
+						//terminal.js hack to display username in chat
+						let userNameInput = document.createElement("span");
+						userNameInput.id = "username";
+						userNameInput.innerHTML = `${username}: `;
+						let currentLine = document
+							.getElementsByClassName("Terminal")[0]
+							.getElementsByTagName("div")[0]
+							.getElementsByTagName("p")[1];
+						currentLine.insertBefore(
+							userNameInput,
+							currentLine.getElementsByTagName("span")[0]
+						);
+						let channelName = event.data.substr(9);
+						chat.clear();
+						welcome(channelName);
+					} else {
+						chat.clear();
+						login();
+					}
+				};
+				chatSocket.send("/passwd " + output);
+			});
+		}
+	});
+}
+
+function welcome(channelName) {
+	chat.print("welcome to 😩");
+	chat.print("---");
+	chat.print("commands:");
+	chat.print("/name to change name (e.g. /name rawrower)");
+	chat.print("/passwd to change password (e.g. /passwd hunter2)");
+	chat.print("/join or /j to join a different channel (e.g. /j general)");
+	chat.print("/w to whisper to someone (e.g. /w rawrower)");
+	chat.print(
+		"/default to automatically join the current channel after logging in"
+	);
+	chat.print("/night to switch between day and night mode");
+
+	//utterly useless night mode flourish
+	for (let e of document.getElementById("chat").getElementsByTagName("div")) {
+		if (e.textContent === "/night to switch between day and night mode") {
+			e.style.display = "inline-block";
+			e.classList.add("night");
+		}
+	}
+
+	joining(channelName);
+	loggedIn();
+}
+
+function joining(channelName) {
+	chat.print("---");
+	chat.print("// now joining #" + channelName);
+	currentChannel = channelName;
+	chat.print("---");
+	chatSocket.send("logs???");
+	chatSocket.send("/list");
+}
+
+function loggedIn() {
+	chatSocket.onmessage = event => {
+		if (event.data.startsWith("[")) {
+			let newMsg = document.createElement("div"); //terminal.js hack to display received messages
+			const timeStampRegex = /\[(\d+)\]/;
+			let timeStamp = new Date();
+			let timeStampRegexOutput = event.data.match(timeStampRegex)[0]; //this whole segment extracts the timestamp from the server message and parses it
+			timeStampRegexOutput = timeStampRegexOutput.substr(
+				1,
+				timeStampRegexOutput.length - 2
+			);
+			timeStamp.setTime(timeStampRegexOutput);
+			let timeStampNow = new Date();
+			let timeStampOutput;
+			if (timeStamp.getDate() !== timeStampNow.getDate()) {
+				//compares timestamp to now to figure out whether to add day and month
+				timeStampOutput = `(${("0" + (timeStamp.getDate() + 1)).substr(-2)}.${(
+					"0" +
+					(timeStamp.getMonth() + 1)
+				).substr(-2)}) [${("0" + timeStamp.getHours()).substr(-2)}:${(
+					"0" + timeStamp.getMinutes()
+				).substr(-2)}]`;
+			} else {
+				timeStampOutput = `[${("0" + timeStamp.getHours()).substr(-2)}:${(
+					"0" + timeStamp.getMinutes()
+				).substr(-2)}]`;
+			}
+			newMsg.innerHTML = event.data.replace(timeStampRegex, timeStampOutput);
+			document
+				.getElementsByClassName("Terminal")[0]
+				.getElementsByTagName("div")[0]
+				.getElementsByTagName("p")[0]
+				.appendChild(newMsg);
+			if (!document.hasFocus()) document.title = "[!] 😩";
+		} else if (event.data.startsWith("channel: ")) {
+			joining(event.data.substr(9));
+		} else if (event.data === "same channel") {
+			chat.print("---");
+			chat.print("// you are already here");
+			chat.print("---");
+		} else if (event.data.startsWith("name changed to ")) {
+			chat.print("---");
+			chat.print("// name changed to " + event.data.substr(16));
+			chat.print("---");
+			document.getElementById("username").innerHTML = document
+				.getElementById("username")
+				.innerHTML.replace(username, event.data.substr(16));
+			username = event.data.substr(16);
+		} else if (event.data === "same name") {
+			chat.print("---");
+			chat.print("// name already taken");
+			chat.print("---");
+		} else if (event.data === "password changed") {
+			chat.print("---");
+			chat.print("// password changed");
+			chat.print("---");
+		} else if (event.data === "default channel set") {
+			chat.print("---");
+			chat.print("// changed default channel to #" + currentChannel);
+			chat.print("---");
+		} else if (event.data.startsWith("list: ")) {
+			userlist.clear();
+			event.data
+				.substr(6)
+				.split(" ")
+				.forEach(element => {
+					userlist.print(element);
+				});
+			if (!document.hasFocus()) document.title = "[!] 😩";
+		}
+	};
+
+	chat.input("", output => {
+		let lastLine = document
+			.getElementsByClassName("Terminal")[0]
+			.getElementsByTagName("div")[0]
+			.getElementsByTagName("p")[0].lastChild; //terminal.js hack to output sent message
+
+		if (lastLine.innerHTML && !lastLine.innerHTML.startsWith("/")) {
+			chatSocket.send("/msg " + output.substr(0, 139));
+
+			lastLine.innerHTML = `[${getTimeStamp()}] ${username}: ${lastLine.innerHTML.substr(
+				0,
+				139
+			)}`;
+		} else {
+			if (lastLine.innerHTML && lastLine.innerHTML !== "/night") {
+				chatSocket.send(output.substr(0, 139));
+			} else if (lastLine.innerHTML === "/night") {
+				document.getElementById("content").classList.contains("night")
+					? document.getElementById("content").classList.remove("night")
+					: document.getElementById("content").classList.add("night");
+			}
+
+			lastLine.innerHTML = "";
+		}
+
+		loggedIn();
+	});
+}
+
+function disconnected() {
+	chat.print("---");
+	chat.print("// disconnected from server");
+	chat.print("// reconnecting");
+	chat.print("---");
+	if (document.getElementById("username"))
+		document.getElementById("username").parentElement.innerHTML = "";
+	setInterval(() => {
+		document.location = location.href;
+	}, 5000);
+}
+
+window.onload = () => {
+	document.getElementById("chat").appendChild(chat.html);
+	document.getElementById("userList").appendChild(userlist.html);
+
+	chatSocket.onopen = login();
+
+	chatSocket.onclose = () => {
+		chatSocket.close();
+		console.log("closed ws");
+		disconnected();
+	};
+
+	document.addEventListener("focusin", () => {
+		document.title = "😩";
+	});
+};
